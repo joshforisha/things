11dfe89e5c775d0a3d80ef97468a0c8e5f9e8e6d
Josh Forisha <josh@ario.com>
2021-02-27 11:49:18 -0600

    Initial commit

---
+++ .vimrc
@@ -0,0 +1,2 @@
+let g:ale_fixers = { 'typescript': ['deno'] }
+let g:ale_linters = { 'typescript': ['deno'] }
---
+++ main.ts
@@ -0,0 +1,9 @@
+import * as flags from "https://deno.land/std/flags/mod.ts";
+import { serve } from "https://deno.land/std/http/server.ts";
+
+const port = flags.parse(Deno.args).port || 8080;
+const server = serve({ port });
+
+for await (const req of server) {
+  req.respond({ body: "Hello world!\n" });
+}
